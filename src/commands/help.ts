9724c4cd3c8ee12e6fab0a3956f1e826d5dbf249
Green_Lab <greencoder001@outlook.com>
2023-05-20 18:26:57 +0200

    Update help.ts

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -4,6 +4,5 @@ export default function command (args: string[]) {
     console.log(chalk.gray('If you want to learn more about AIPak: https://github.com/greencoder001/aipak/'))
     console.log(chalk.cyan('\nCommands:'))
     console.log(chalk.cyan(`install <program name> ${chalk.gray('Installs a program')}`))
-    console.log(chalk.cyan(`uninstall <program name> ${chalk.gray('Unnstalls a program')}`))
     console.log(chalk.cyan(`update ${chalk.gray('Updates aipak')}`))
 }
